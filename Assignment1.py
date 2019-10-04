f8f560ac26702ba16cb9f7ebf57e75c8eaa9b957
Jackson Zhaoxiong Ding <zding5@u.rochester.edu>
2019-10-03 21:46:34 -0400

    removed data successfully, now everything is just children

--- Assignment1.py
+++ Assignment1.py
@@ -5,6 +5,131 @@
 import pdb
 import sys
 
+### WILL
+PUNCTUATION_TAGS = [',', ':', '.', '``', '\'\'', '-LRB-', '-RRB', '-LSB-', '-RSB-', '$', '#']
+
+# A subset of the phrase type tags recognized by the Penn Treebank. See the
+# README for more details on how the subset was selected.
+PHRASE_TYPES = ['S', 'NP', 'VP', 'PP', 'ADJP', 'ADVP', 'SBAR', 'SBARQ', 'SQ', 'WHADJP', 'WHADVP', 'WHPP']
+
+def read_data(fp):
+	"""Reads in a file containing a phrase structure parse as a single string
+
+	Arguments:
+	  fp -- Path to the file to be read in
+
+	Return Values:
+	  s -- The phrase structure parse as a string
+	"""
+	file = open(fp, 'r')
+	s = ''
+	for line in file:
+		s += line.strip()
+	return s
+
+def parenthesize(s):
+	"""Parenthesizes a string
+
+	Arguments:
+	  s -- The string to parenthesize
+
+	Return Values:
+	  The parenthesized string
+	"""
+	return '(' + s + ')'
+
+def index_words(s, idx=1):
+	"""Appends indices to words in a phrase structure parse based on their order in
+   the sentence.
+
+   Arguments:
+     s -- The phrase structure parse to be indexed (a string)
+     idx -- The highest index that has been already been applied to a word in
+       the parse. The indexing starts at 1 by default.
+
+   Return values:
+     indexed_constituents_str -- A string consisting of the indexed phrase
+       structure parse
+     indexed_constituents -- A list of lists (whose structure reflects the
+       nesting structure of the PST), with all words indexed.
+     idx -- Same as in the arguments.
+	"""
+	# If no paren is present in the string, it means
+	# we've hit a pair of atoms (a tag and a word).
+	if '(' not in s:
+
+		# Identify the tag and the word
+		tag, word = s.split()
+
+		# If the token is a puncutation mark, we shouldn't index it 
+		# (or increment the index counter), so just return them as is
+		if tag in PUNCTUATION_TAGS:
+			return parenthesize(s), [s], idx
+
+		# Otherwise, we have a token that's *not* a punctuation mark, which
+		# means we need to index it and increment the index counter.
+		else:
+			return parenthesize(s + '-' + str(idx)), [s + '-' + str(idx)], idx + 1
+
+	# A paren in the string indicates that we still have a phrase that needs to
+	# be parsed. We now need to determine the constituents of the phrase. 		
+	start_idx = s.index('(')
+	tag = s[:start_idx]
+
+	# A list to hold all of the constituents of the current phrase. This will
+	# actually end up being a list of lists of varying depths as the recursive
+	# calls pile up.
+	constituents = []
+
+	# A string for capturing a constituent between parentheses
+	expression = ''
+
+	# A counter to keep track of the number of unmatched parentheses
+	unmatched_count = 0
+
+    # Iterate over the string, matching parentheses.  
+	for i,c in enumerate(s[start_idx:]):
+
+		# Found a closing paren, so decrement the unmatched count
+		if c == ')':
+			unmatched_count -= 1
+
+		# If the number of unmatched parens is greater than 0, it means we're
+		# still building out the current constituent, so append the current
+		# character to that constituent.	
+		if unmatched_count > 0:
+			expression += c
+
+		# Found an open paren, so increment the unmatched count	
+		if c == '(':
+			unmatched_count += 1
+
+		# All parens have been matched, which means we've found a complete
+		# constituent. Append the constituent to the list of all constituents
+		# in the phrase and reset the builder string.
+		# TODO: explain why isspace is necessary
+		if unmatched_count == 0 and not c.isspace():
+			constituents.append(expression)
+			expression = ''
+
+	# Constituents that have been indexed		
+	indexed_constituents = [tag]
+	indexed_constituents_str = tag
+
+	# Recurse on each constituent that was found, appending the results to the
+	# list of already-indexed constituents.
+	for constituent in constituents:
+		indexed_str, indexed, idx = index_words(constituent, idx)
+		indexed_constituents.append(indexed)
+		indexed_constituents_str += indexed_str
+
+    # When we're at the root, don't double-parenthesize the whole parse
+	if tag != 'ROOT':
+		indexed_constituents_str = parenthesize(indexed_constituents_str)
+
+	return indexed_constituents_str, indexed_constituents, idx
+### WILL
+
 class Tree:
     def __init__(self, child=[], thisType=None, data=[]):
         self.child = child
@@ -41,6 +166,23 @@ class Tree:
         for child in currChild:
             child.parse()
         self.child = currChild
+
+    def addChild(self,node):
+        self.child.append(node)
+    def parse2(self,currList):
+        if len(currList)>0:
+            # currList = inputList
+            
+            self.type = currList[0]
+            print(self.type)
+            # pdb.set_trace()
+            for nextInput in currList[1:]:
+                # pdb.set_trace()
+                newNode = Tree()
+                newNode.child = [] #????
+                newNode.parse2(nextInput)
+                self.child.append(newNode)
+                # self.child+=newNode
     
     def createPhrases(self):
         if len(self.data)>0:
@@ -118,23 +260,25 @@ class Phrase:
 
 if __name__ == "__main__":
     ## FILE INPUT
-    f = open(sys.argv[1])
-    lines = f.readlines()
-    f.close()
+    # f = open(sys.argv[1])
+    # lines = f.readlines()
+    # f.close()
 
-    spacing = 2
+    # spacing = 2
 
-    ## BASIC PARSING
-    # Parse into: [# Space][TYPE][REST]
-    parsedLines = []
-    for line in lines:
-        currLength = len(line.split('(')[0])
-        currParse = [None]*3
-        currParse[0] = currLength
-        currParse[1] = line.split('(')[1].strip()
-        currParse[2] = line.split('(')[2:]
-        parsedLines.append(currParse)
+    # ## BASIC PARSING
+    # # Parse into: [# Space][TYPE][REST]
+    # parsedLines = []
+    # for line in lines:
+    #     currLength = len(line.split('(')[0])
+    #     currParse = [None]*3
+    #     currParse[0] = currLength
+    #     currParse[1] = line.split('(')[1].strip()
+    #     currParse[2] = line.split('(')[2:]
+    #     parsedLines.append(currParse)
 
+    s = read_data(sys.argv[1])
+    indexed_constituents_str, indexed_constituents, idx = index_words(s)
     ## DATA
 
 
@@ -142,12 +286,12 @@ if __name__ == "__main__":
     ## FUNCTION CALLS
     # Create and Parse Tree
     x = Tree()
-    x.parse()
-    x.createPhrases()
-    x.indexWords()
+    x.parse2(indexed_constituents[1])
+    # x.createPhrases()
+    # x.indexWords()
 
     # AS2
-    # pdb.set_trace()
+    pdb.set_trace()
     # x.semanticParse()
 
     # Tests
